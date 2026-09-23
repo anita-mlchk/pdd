bdf78bf8f1cfc2468edf66e511e8cede22674dbb
Anita Malchuk <anitamalchuk@gmail.com>
2026-09-23 09:08:10 +0300

    Fix syntax errors in server.js and models

--- models/ticket.js
+++ models/ticket.js
@@ -1,34 +1,18 @@
 'use strict';
 const { Model } = require('sequelize');
-
 module.exports = (sequelize, DataTypes) => {
   class Ticket extends Model {
     static associate(models) {
-      // Описание связей
     }
   }
   Ticket.init({
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    category: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    questionsCount: {
-      type: DataTypes.INTEGER,
-      defaultValue: 20
-    },
-    timeLimitMinutes: {
-      type: DataTypes.INTEGER,
-      defaultValue: 20
-    },
-    description: DataTypes.STRING
+    title: DataTypes.STRING,
+    category: DataTypes.STRING,
+    questionsCount: DataTypes.INTEGER,
+    timeLimitMinutes: DataTypes.INTEGER,
   }, {
     sequelize,
     modelName: 'Ticket',
-    tableName: 'Tickets'
   });
   return Ticket;
 };
